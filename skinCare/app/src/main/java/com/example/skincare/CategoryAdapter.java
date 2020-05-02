cc2250bced72249343bd6425d0d5f30336484951
admin <admin@MacBook-Pro-10.home>
2020-05-02 15:19:11 -0400

    movies

---
+++ skinCare/app/src/main/java/com/example/skincare/CategoryAdapter.java
@@ -0,0 +1,4 @@
+package com.example.skincare;
+
+public class CategoryAdapter {
+}
---
+++ skinCare/app/src/main/java/com/example/skincare/DrySkin.java
@@ -0,0 +1,14 @@
+package com.example.skincare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrySkin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dry_skin);
+    }
+}
